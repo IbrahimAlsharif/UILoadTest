f4753d11d1771d75bf0988e0b53a8e2b81b19f27
ibrahim <isharif@famcare.app>
2022-12-22 10:43:10 +0200

    Update names

--- src/test/java/UILoadTest/MainTest.java
+++ src/test/java/UILoadTest/MainTest.java
@@ -8,15 +8,15 @@ public class MainTest {
 
     public static void main(String[] args) {
         int numberOfUsersPerSession = 20;
-        String[] listOfSessionsUrls = new String[]{
+        String[] listOfUrls = new String[]{
                 "https://bbb.famcare.app/b/ahm-hb6-gsm-akr",
                 "https://bbb.famcare.app/b/ahm-rbq-u3t-jcm",
                 "https://bbb.famcare.app/b/ahm-blk-xne-0tj",
                 "https://bbb.famcare.app/b/ahm-yro-5km-60r",
                 "https://bbb.famcare.app/b/ahm-zky-pbt-xjq"
         };
-        ExecutorService executor = Executors.newFixedThreadPool(listOfSessionsUrls.length);
-        for (String sessionUrl : listOfSessionsUrls) {
+        ExecutorService executor = Executors.newFixedThreadPool(listOfUrls.length);
+        for (String sessionUrl : listOfUrls) {
             executor.execute(new UsersGenerator(sessionUrl, numberOfUsersPerSession));
             try {
                 Thread.sleep(2000);
