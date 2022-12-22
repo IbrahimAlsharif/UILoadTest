50ed285ca2807af9361fcb6a245c66fad07c3e0a
ibrahim <isharif@famcare.app>
2022-12-22 10:42:34 +0200

    20

--- src/test/java/UILoadTest/MainTest.java
+++ src/test/java/UILoadTest/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
 
 
     public static void main(String[] args) {
-        int numberOfUsersPerSession = 1;
+        int numberOfUsersPerSession = 20;
         String[] listOfSessionsUrls = new String[]{
                 "https://bbb.famcare.app/b/ahm-hb6-gsm-akr",
                 "https://bbb.famcare.app/b/ahm-rbq-u3t-jcm",
